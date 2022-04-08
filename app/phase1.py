13e40511a31a4058477826deeb3984c1b2ef4c67
Michał <michal.stachura@santri.pl>
2022-04-08 10:53:38 +0200

    branch can be deleted

--- app/phase1.py
+++ app/phase1.py
@@ -8,42 +8,62 @@ from datetime import datetime, timedelta
 from app.utils import Report
 
 
+def multiprocessing_worker(i, phase_1_sub_duration, phase_1_details):
+    subprocess_start = datetime.now()
+    phase_1_details.append(
+        f"Task: {i} (start) - PID: {len(psutil.pids())} CPU: {psutil.cpu_percent(interval=None)}%, RAM (GB): avl: {round(psutil.virtual_memory().available/1073741824, 2)}, used: {round(psutil.virtual_memory().used/1073741824, 2)}, {psutil.virtual_memory().percent}%)"
+    )
+
+    res = requests.get(
+        "https://thispersondoesnotexist.com/image", stream=True)
+    image_name = f"img{i}.jpg"
+    if res.status_code == 200:
+        res.raw.decode_content = True
+        with open(f"downloads/{image_name}", "wb") as file:
+            shutil.copyfileobj(res.raw, file)
+
+    subprocess_end = datetime.now()
+    phase_1_sub_duration.append(subprocess_end - subprocess_start)
+    phase_1_details.append(
+        f"Task: {i} (end) - PID: {len(psutil.pids())} CPU: {psutil.cpu_percent(interval=None)}%, RAM (GB): avl: {round(psutil.virtual_memory().available/1073741824, 2)}, used: {round(psutil.virtual_memory().used/1073741824, 2)}, {psutil.virtual_memory().percent}%)")
+
+
 class PhaseOne(Report):
     phase_1_sub_duration = []
     phase_1_details = []
     url = "https://thispersondoesnotexist.com/image"
     start = datetime.now()
 
-    def __worker(self, i):
-        subprocess_start = datetime.now()
-        self.phase_1_details.append(
-            self.generate_details(
-                i,
-                psutil.pids(),
-                psutil.cpu_percent(interval=None),
-                psutil.virtual_memory(),
-                subprocess_start
-            )
-        )
+    # def __worker(self, i):
+    #     subprocess_start = datetime.now()
+    #     self.phase_1_details.append(
+    #         self.generate_details(
+    #             i,
+    #             psutil.pids(),
+    #             psutil.cpu_percent(interval=None),
+    #             psutil.virtual_memory(),
+    #             subprocess_start
+    #         )
+    #     )
 
-        res = requests.get(self.url, stream=True)
-        image_name = f"img{i}.jpg"
-        if res.status_code == 200:
-            res.raw.decode_content = True
-            with open(f"downloads/{image_name}", "wb") as file:
-                shutil.copyfileobj(res.raw, file)
+    #     res = requests.get(self.url, stream=True)
+    #     image_name = f"img{i}.jpg"
+    #     if res.status_code == 200:
+    #         res.raw.decode_content = True
+    #         with open(f"downloads/{image_name}", "wb") as file:
+    #             shutil.copyfileobj(res.raw, file)
 
-        subprocess_end = datetime.now()
-        self.phase_1_sub_duration.append(
-            self.get_time_diff(subprocess_start, subprocess_end))
-        self.phase_1_details.append(
-            self.generate_details(
-                i,
-                psutil.pids(),
-                psutil.cpu_percent(interval=None),
-                psutil.virtual_memory()
-            )
-        )
+    #     subprocess_end = datetime.now()
+    #     self.phase_1_sub_duration.append(
+    #         self.get_time_diff(subprocess_start, subprocess_end))
+    #     self.phase_1_details.append(
+    #         self.generate_details(
+    #             i,
+    #             psutil.pids(),
+    #             psutil.cpu_percent(interval=None),
+    #             psutil.virtual_memory()
+    #         )
+    #     )
 
     def common_images_download(self, no_of_images):
         for i in range(0, no_of_images):
@@ -55,26 +75,6 @@ class PhaseOne(Report):
                 executor.submit(self.__worker, i)
 
     def multprocessing_images_download(self, no_of_images, max_workers=10):
-        global worker
-
-        def worker(i, phase_1_sub_duration, phase_1_details):
-            subprocess_start = datetime.now()
-            phase_1_details.append(
-                f"Task: {i} (start) - PID: {len(psutil.pids())} CPU: {psutil.cpu_percent(interval=None)}%, RAM (GB): avl: {round(psutil.virtual_memory().available/1073741824, 2)}, used: {round(psutil.virtual_memory().used/1073741824, 2)}, {psutil.virtual_memory().percent}%)"
-            )
-
-            res = requests.get(
-                "https://thispersondoesnotexist.com/image", stream=True)
-            image_name = f"img{i}.jpg"
-            if res.status_code == 200:
-                res.raw.decode_content = True
-                with open(f"downloads/{image_name}", "wb") as file:
-                    shutil.copyfileobj(res.raw, file)
-
-            subprocess_end = datetime.now()
-            phase_1_sub_duration.append(subprocess_end - subprocess_start)
-            phase_1_details.append(
-                f"Task: {i} (end) - PID: {len(psutil.pids())} CPU: {psutil.cpu_percent(interval=None)}%, RAM (GB): avl: {round(psutil.virtual_memory().available/1073741824, 2)}, used: {round(psutil.virtual_memory().used/1073741824, 2)}, {psutil.virtual_memory().percent}%)")
 
         with multiprocessing.Manager() as manager:
             phase_1_sub_duration = manager.list()  # Can be shared between processes
@@ -82,7 +82,7 @@ class PhaseOne(Report):
 
             pool = multiprocessing.Pool(max_workers)
             for i in range(0, no_of_images):
-                pool.apply_async(worker, args=(
+                pool.apply_async(multiprocessing_worker, args=(
                     i, phase_1_sub_duration, phase_1_details))
 
             pool.close()
